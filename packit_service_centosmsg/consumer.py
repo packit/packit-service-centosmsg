31ccd97fe300d3a0b2261bebe1e45bc015e64696
Jan Sakalos <sakalosj@gmail.com>
2020-03-04 12:45:34 +0100

    initial commit

---
+++ packit_service_centosmsg/consumer.py
@@ -0,0 +1,90 @@
+# MIT License
+#
+# Copyright (c) 2018-2019 Red Hat, Inc.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import json
+import os
+import ssl
+import sys
+from logging import getLogger
+from os import getenv, path
+from pprint import pprint
+from time import sleep
+
+import paho.mqtt.client as mqtt
+from celery import Celery
+
+logger = getLogger(__name__)
+logger.setLevel("INFO")
+
+
+print(sys.path)
+class Consumerino(mqtt.Client):
+    """
+    Consume events from centos messaging
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._celery_app = None
+
+    @property
+    def celery_app(self):
+        if self._celery_app is None:
+            redis_host = getenv("REDIS_SERVICE_HOST", "localhost")
+            redis_port = getenv("REDIS_SERVICE_PORT", "6379")
+            redis_db = getenv("REDIS_SERVICE_DB", "0")
+            redis_url = "redis://{host}:{port}/{db}".format(
+                host=redis_host, port=redis_port, db=redis_db
+            )
+
+            self._celery_app = Celery(backend=redis_url, broker=redis_url)
+        return self._celery_app
+
+    def on_message(self, client, userdata, msg):
+        pprint(msg.topic)
+        pprint(json.loads(msg.payload))
+        # self.celery_app.send_task(
+            # name="task.steve_jobs.process_message", kwargs={"event": msg}
+        # )
+        logger.info(json.loads(msg.payload))
+
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        logger.info(f"Connected wit result code: ${str(rc)}")
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("git.stg.centos.org/#")
+
+    def consume_from_centos_messaging(self, ca_certs: str, certfile: str):
+
+        if not (path.isfile(ca_certs) and path.isfile(certfile)):
+            raise FileNotFoundError
+
+        self.tls_set(ca_certs=ca_certs, certfile=certfile,
+                     keyfile=certfile, cert_reqs=ssl.CERT_REQUIRED,
+                     tls_version=ssl.PROTOCOL_TLS)
+        self.connect(host="mqtt.stg.centos.org", port=8883)
+        self.loop_forever()
+
+

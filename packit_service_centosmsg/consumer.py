330aa28298a9d2ae3af8180f018478eeeaf9f5fc
Jiri Popelka <jpopelka@redhat.com>
2021-10-01 12:29:01 +0200

    Remove AWS SQS support
    reverts 683ae83
    See also https://github.com/packit/deployment/issues/226

--- packit_service_centosmsg/consumer.py
+++ packit_service_centosmsg/consumer.py
@@ -49,24 +49,14 @@ class Consumerino(mqtt.Client):
     @property
     def celery_app(self):
         if self._celery_app is None:
-            bt_options = {}
-            if getenv("AWS_ACCESS_KEY_ID") and getenv("AWS_SECRET_ACCESS_KEY"):
-                broker_url = "sqs://"
-                if not getenv("QUEUE_NAME_PREFIX"):
-                    raise ValueError("QUEUE_NAME_PREFIX not set")
-                bt_options["queue_name_prefix"] = getenv("QUEUE_NAME_PREFIX")
-            elif getenv("REDIS_SERVICE_HOST"):
-                host = getenv("REDIS_SERVICE_HOST")
-                password = getenv("REDIS_PASSWORD", "")
-                port = getenv("REDIS_SERVICE_PORT", "6379")
-                db = getenv("REDIS_SERVICE_DB", "0")
-                broker_url = f"redis://:{password}@{host}:{port}/{db}"
-            else:
-                raise ValueError("Celery broker not configured")
+            host = getenv("REDIS_SERVICE_HOST", "redis")
+            password = getenv("REDIS_PASSWORD", "")
+            port = getenv("REDIS_SERVICE_PORT", "6379")
+            db = getenv("REDIS_SERVICE_DB", "0")
+            broker_url = f"redis://:{password}@{host}:{port}/{db}"
+            logger.debug(f"Celery uses {broker_url}")
 
             self._celery_app = Celery(broker=broker_url)
-            self._celery_app.conf.broker_transport_options = bt_options
-            logger.debug(f"Celery uses {broker_url} with {bt_options}")
             if getenv("CELERY_DEFAULT_QUEUE"):
                 self._celery_app.conf.task_default_queue = getenv(
                     "CELERY_DEFAULT_QUEUE"
